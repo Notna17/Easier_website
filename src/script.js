bf62f17060eee11f6dfb24023b25692ab7363206
Notna17 <tonketi2007@gmail.com>
2025-04-01 16:56:19 +0300

    Дошлівував, щоб виконувалися вимоги

--- src/script.js
+++ src/script.js
@@ -18,3 +18,14 @@ document.getElementById("download_way2").addEventListener("click", () => {
 document.getElementById("download_way3").addEventListener("click", () => {
     downloads += 1;
 });
+let showSublists = (x) => {
+    let listItem = document.getElementById("leftlistitem" + x.toString());
+    let sublist = document.getElementById("sublist" + x.toString());
+    let hidden = true;
+    listItem.addEventListener("mouseenter", () => {
+        if (hidden) {sublist.style.display = "block";}
+        else {sublist.style.display = "none";}
+        hidden = !hidden;
+    });
+}
+for (let i = 1; i <= 6; i++) {showSublists(i)}
